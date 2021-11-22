f626d71c1ea33dd7b6e44ad50243fa4043c4d398
SolanaEngineer <topstack2021a@gmail.com>
2021-11-22 15:24:50 +0300

    fix/bugs, demo

--- app/src/pages/PageHome.tsx
+++ app/src/pages/PageHome.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import * as anchor from '@project-serum/anchor';
 import { useWallet, WalletContextState } from '@solana/wallet-adapter-react';
-import { borrowUSDr, createGlobalState, createTokenVault, createUserTrove, depositCollateral, repayCollateral, repayUSDr, withdrawCollateral } from '../actions';
+import { borrowUSDr, createGlobalState, createTokenVault, createUserTrove, depositCollateral,  repayUSDr, withdrawCollateral } from '../actions';
 import { Button } from '@material-ui/core';
 const connection = new anchor.web3.Connection('https://api.devnet.solana.com');
 
@@ -9,56 +9,49 @@ const PageHome : React.FC = () => {
   const wallet:WalletContextState = useWallet();
   const [dispInfo, setDispInfo] = useState('transaction result:');
 
-  async function createGlobalState() {
+  async function createGlobalStateUI() {
     if(wallet.connected){
       const demoLog = await createGlobalState(connection, wallet);
       setDispInfo(demoLog);
     }
     else{     setDispInfo("connect your wallet");    }
   }
-  async function createTokenVault() {
+  async function createTokenVaultUI() {
     if(wallet.connected){
       const demoLog = await createTokenVault(connection, wallet);
       setDispInfo(demoLog);
     }
     else{     setDispInfo("connect your wallet");    }
   }
-  async function createUserTrove() {
+  async function createUserTroveUI() {
     if(wallet.connected){
       const demoLog = await createUserTrove(connection, wallet);
       setDispInfo(demoLog);
     }
     else{     setDispInfo("connect your wallet");    }
   }
-  async function depositCollateral() {
+  async function depositCollateralUI() {
     if(wallet.connected){
       const demoLog = await depositCollateral(connection, wallet, 1 * 1000000000);
       setDispInfo(demoLog);
     }
     else{     setDispInfo("connect your wallet");    }
   }
-  async function repayCollateral() {
-    if(wallet.connected){
-      const demoLog = await repayCollateral(connection, wallet, 0.2 * 1000000000);
-      setDispInfo(demoLog);
-    }
-    else{     setDispInfo("connect your wallet");    }
-  }
-  async function withdrawCollateral() {
+  async function withdrawCollateralUI() {
     if(wallet.connected){
       const demoLog = await withdrawCollateral(connection, wallet, 1 * 1000000000);
       setDispInfo(demoLog);
     }
     else{     setDispInfo("connect your wallet");    }
   }
-  async function borrowUSDr() {
+  async function borrowUSDrUI() {
     if(wallet.connected){
       const demoLog = await borrowUSDr(connection, wallet, 50 * 1000000);
       setDispInfo(demoLog);
     }
     else{     setDispInfo("connect your wallet");    }
   }
-  async function repayUSDr() {
+  async function repayUSDrUI() {
     if(wallet.connected){
       const demoLog = await repayUSDr(connection, wallet,  20 * 1000000);
       setDispInfo(demoLog);
@@ -71,32 +64,29 @@ const PageHome : React.FC = () => {
     >
     <br />
     <br />
-    <Button size="medium" color="primary" variant="outlined" onClick={e => createGlobalState()}>
+    <Button size="medium" color="primary" variant="outlined" onClick={e => createGlobalStateUI()}>
       Create Program State
     </Button>
-    <Button size="medium" color="primary" variant="outlined" onClick={e => createTokenVault()}>
+    <Button size="medium" color="primary" variant="outlined" onClick={e => createTokenVaultUI()}>
       Create SOL-STEP Pool
     </Button>
-    <Button size="medium" color="primary" variant="outlined" onClick={e => createUserTrove()}>
+    <Button size="medium" color="primary" variant="outlined" onClick={e => createUserTroveUI()}>
       Create SOL-STEP User Account
     </Button>
     <br />
     <br />
-    <Button size="medium" color="primary" variant="outlined" onClick={e => depositCollateral()}>
+    <Button size="medium" color="primary" variant="outlined" onClick={e => depositCollateralUI()}>
       Deposit SOL-STEP
     </Button>
-    <Button size="medium" color="primary" variant="outlined" onClick={e => repayCollateral()}>
-      Repay SOL-STEP
-    </Button>
-    <Button size="medium" color="primary" variant="outlined" onClick={e => withdrawCollateral()}>
+    <Button size="medium" color="primary" variant="outlined" onClick={e => withdrawCollateralUI()}>
       Withdraw SOL-STEP
     </Button>
     <br />
     <br />
-    <Button size="medium" color="primary" variant="outlined" onClick={e => borrowUSDr()}>
+    <Button size="medium" color="primary" variant="outlined" onClick={e => borrowUSDrUI()}>
       Borrow USDr
     </Button>
-    <Button size="medium" color="primary" variant="outlined" onClick={e => repayUSDr()}>
+    <Button size="medium" color="primary" variant="outlined" onClick={e => repayUSDrUI()}>
       Repay USDr with debt
     </Button>
     <br />
